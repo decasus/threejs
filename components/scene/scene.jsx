3860462356a09027e8ef267b78a0830cd6054f5c
Anton <toozexx@gmail.com>
2022-09-08 08:25:46 +0300

    ...

--- components/scene/scene.jsx
+++ components/scene/scene.jsx
@@ -8,28 +8,29 @@ import {
     WebGLRenderer, ShaderMaterial, Vector2
 } from 'three';
 import {useEffect} from "react";
-import {IfcDateTime} from "three/addons/loaders/ifc/web-ifc-api";
+import {OrbitControls} from "three/addons/controls/OrbitControls";
 
 const SceneComponent = () => {
 
     useEffect(() => {
         const canvas = document.querySelector('#threeCanvas');
+
+        const scene = new Scene();
+        const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         const renderer = new WebGLRenderer({canvas});
         renderer.setSize(window.innerWidth, window.innerHeight);
 
-        const camera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
-
         camera.position.z = 2;
 
-        const scene = new Scene();
+        scene.fog = new Fog('white', 0.1, 1);
+        scene.background = new Color(0x1c1c1c);
 
-        scene.fog = new Fog('black', 0.1, 2);
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.update();
 
         const geometry = new BoxGeometry(1, 1, 1);
 
-        let uniforms = {
-            colorB: {type: 'vec3', value: new Color('orange')},
-            colorA: {type: 'vec3', value: new Color('yellow')},
+        const uniforms = {
             time: {type: 'float', value: 0}
         }
 
@@ -43,51 +44,54 @@ const SceneComponent = () => {
 
         scene.add(cube);
 
-        function render(time) {
-            time *= 0.001;
+        function animate() {
+            requestAnimationFrame(animate);
             uniforms.time.value += 0.01;
-            cube.rotation.x = time;
-            cube.rotation.y = time;
+            //cube.rotation.x += 0.01;
+            //cube.rotation.y += 0.01;
+            controls.update();
             renderer.render(scene, camera);
-            requestAnimationFrame(render);
         }
 
-        requestAnimationFrame(render);
+        animate();
 
     }, [])
 
     function vertexShader() {
         return `
-    varying vec3 vUv; 
+    varying vec3 pos; 
 
     void main() {
-      vUv = position; 
-      vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-      gl_Position = projectionMatrix * modelViewPosition; 
+      pos = position; 
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 ); 
     }
   `
     }
 
     function fragmentShader() {
         return `
-      uniform vec3 colorA; 
-      uniform vec3 colorB;
       uniform float time;
-      varying vec3 vUv;
-
+      varying vec3 pos;
+      vec3 col;
+      
       void main() {
-        gl_FragColor = vec4(mix(colorA, colorB, time + vUv.z), 1.0);
+          if(pos.y == 0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(0,2,9)), 1.0);
+          if(pos.y == -0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(4,8,2)), 1.0);
+          if(pos.x == 0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(5,1,7)), 1.0);
+          if(pos.x == -0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(7,9,1)), 1.0);
+          if(pos.z == -0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(6,1,8)), 1.0);
+          if(pos.z == 0.5) gl_FragColor = vec4(0.5 + 0.5*cos(time+vec3(2,0,0)), 1.0);
       }
   `
     }
 
-/*    Результатом должна являтся страница со сценой, на которой фог и куб с материалом
-    без восприимчивости к свету и кастомным шейдером, в котором цвет высчитывается относительно позиции вершины и времени.
-        Импорт theejs должен быть динамический при подключении компонента со сценой
-    При клике на куб он должен анимированно повернуться в случайно
-    выбранной оси на случайную величину
-    После удаления компонента, сцена должна уничтожиться
-*/
+    /*    Результатом должна являтся страница со сценой, на которой фог и куб с материалом
+        без восприимчивости к свету и кастомным шейдером, в котором цвет высчитывается относительно позиции вершины и времени.
+            Импорт theejs должен быть динамический при подключении компонента со сценой
+        При клике на куб он должен анимированно повернуться в случайно
+        выбранной оси на случайную величину
+        После удаления компонента, сцена должна уничтожиться
+    */
 
     return (
         <div>
